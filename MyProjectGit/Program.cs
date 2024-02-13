44e5e2d9beae375644f67409f01e99a5f9cb201c
DmitroTsybak <dmitrot9316@gmail.com>
2024-02-13 20:58:08 +0200

    My first commit for project

---
+++ MyProjectGit/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введіть число 1: ");
+var first=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введіть число 2: ");
+var second=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сума чисел =  {first + second} ");
